06dc17fc3237b81fe9d88b49928ecff7a14c1d76
navincograph179 <navin_gurung@co-graph.com>
2025-02-12 16:09:18 +0900

    variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const myName = "Navin"
+const my_Name = "Tanaka"
+const myname = "Yamada"
+
+const email = "@gmail.com"
+
+
+let age = 12;
+age = 23;
+let number = 10;
+
+
+let my_age = "12"
+my_age = "22"
+let myNumber = "10"
+
+console.table([my_age,myName, my_Name, myname, age, number]);
+
