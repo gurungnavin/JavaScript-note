3bb977d8f030d95ce4279f64c70ad13f5a94b10d
navincograph179 <navin_gurung@co-graph.com>
2025-02-12 17:56:28 +0900

    update dataTypes as code

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -8,3 +8,10 @@ let sav = null  // Stand Alone Value, it is not undefined but empty
 
 let x;   // undefined, value is not defined yet.
 
+const symbol1 = Symbol("id");
+const symbol2 = Symbol("id");
+
+console.log(symbol1 === symbol2);  // false, they are unique
+
+console.log(symbol1.toString()); // "Symbol(id)"
+console.log(symbol2.toString()); // "Symbol(id)"
