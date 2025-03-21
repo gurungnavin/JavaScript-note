7062feab4ac443d50091d4cac65491870282e0dc
navincograph179 <navin_gurung@co-graph.com>
2025-03-21 14:28:09 +0900

    destructuring and json

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -29,27 +29,89 @@
 
 // console.log(user);
 
+// const obj1 = {a: 2, b: 6, c:10};
+// const obj2 = {d: 4, e: 8, f:12};
 
+// const obj3 = {...obj1, ...obj2}; // output: {a: 2, b: 6, c: 10, d: 4, e: 8, f: 12}
+// const obj4 = Object.assign(obj1, obj2); // output: {a: 2, b: 6, c: 10, d: 4, e: 8, f: 12}
 
-const obj1 = {a: 2, b: 6, c:10}; 
-const obj2 = {d: 4, e: 8, f:12};
+// const users = [
+//   {user1: "John Doe", age: 25},
+//   {user2: "Jane Doe", age: 30},
+//   {
+//     user3: "Alice",
+//     age: 35,
+//   },
+//   {
+//     user4: "Bob",
+//     age: 40,
+//   },
+// ]
 
-const obj3 = {...obj1, ...obj2}; // output: {a: 2, b: 6, c: 10, d: 4, e: 8, f: 12}
-const obj4 = Object.assign(obj1, obj2); // output: {a: 2, b: 6, c: 10, d: 4, e: 8, f: 12}
+// console.log(users[0].user1); // output: John Doe
 
+// Object c
+// Object destructuring is a way to extract properties from an object and assign them to variables.
 
-const users = [
-  {user1: "John Doe", age: 25},
-  {user2: "Jane Doe", age: 30},
-  {
-    user3: "Alice",
-    age: 35,
+const user = {
+  name: "山田太郎",
+  age: 30,
+  address: {
+    city: "東京",
+    state: "東京都",
+    country: "日本",
   },
-  {
-    user4: "Bob",
-    age: 40,
-  },    
-]
+  hobbies: [
+    {
+      reading: ["漫画", "歴史書"],
+    },
+    "プログラミング",
+    "ハイキング",
+  ],
+  friends: {
+    friend1: "佐藤花子",
+    friend2: "鈴木次郎",
+    friend3: "高橋三郎",
+  },
+};
 
+const { name, age, address, hobbies, friends } = user;
 
-console.log(users[0].user1); // output: John Doe
+console.log(`名前: ${name}`); // output: 山田太郎
+console.log(`年齢: ${age}`); // output: 30
+console.log(`住所: ${address.city}都, ${address.country}`); // output: {city: "東京", state: "東京都", country: "日本"}
+
+//JSON
+// JSON (JavaScript Object Notation) is a lightweight data-interchange format.
+
+// {
+//   "name": "山田太郎",
+//   "age": 30,
+//   "address": {
+//     "city": "東京",
+//     "state": "東京都",
+//     "country": "日本"
+//   }
+// }
+
+// API Data with array of objects in JSON format
+// const company = [
+//   {
+//     id: 1,
+//     name: "Google",
+//     location: "Mountain View, CA",
+//     employees: 100000,
+//   },
+//   {
+//     id: 2,
+//     name: "Facebook",
+//     location: "Menlo Park, CA",
+//     employees: 50000,
+//   },  
+//   {
+//     id: 3,
+//     name: "Apple",
+//     location: "Cupertino, CA",
+//     employees: 150000,
+//   },
+// ];
